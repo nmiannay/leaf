bc9df57dde8d771cf2e5bb4cc00ddcc7fdb14ecd
miannay-nicolas <nicolas.miannay@gmail.com>
2013-12-29 13:28:58 +0100

    Update test files

--- Views/view.php
+++ Views/view.php
@@ -1,18 +1,32 @@
+doctype html
 html
-  - $test = 'test'
   head
-    title Hello world
-      | plop
-  body class="titi" class="0"
-    #ps
-      | test
-      - if $test == 'test'
-        span span1
-          span span2
-      - foreach str_split($test) as $char
+    title Slim Examples
+    meta name="keywords" content="template language"
+    meta name="author" content="author"
+    javascript:
+      | alert('Slim supports embedded javascript!')
+
+  body
+    h1 Markup examples
+    - $items = array(1)
+    #content
+      p This example shows you how a basic Slim file looks like.
+
+
+      - if !empty($items)
+        table
+          - foreach $items as $item
+            tr
+              td.name
+                = $item
+              td.price
+                = $item
+      - else
         p
-          = $char
-    div.test
-    @block: testblock
-      p
-    div.test
+          | No items found.  Please add some inventory.
+          | Thank you!
+
+    div id="footer"
+      render 'footer'
+      | Copyright © #{year} #{author}t
