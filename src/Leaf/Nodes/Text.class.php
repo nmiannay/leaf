40f9314b0b0e85a5dffd9f3dc55d8a484c378f87
miannay-nicolas <nicolas.miannay@gmail.com>
2014-02-09 19:45:57 +0100

    Add space between two DomText elements

--- src/Leaf/Nodes/Text.class.php
+++ src/Leaf/Nodes/Text.class.php
@@ -6,7 +6,12 @@ class Text extends \DomText
   public function __toHTML()
   {
     $text = htmlspecialchars($this->nodeValue, ENT_COMPAT | ENT_HTML5, 'UTF-8', false);
-    return (preg_replace('/#{\$(\w+)}/', '<?php echo $$1; ?>', $text));
+    $text = (preg_replace('/#{\$(\w+)}/', '<?php echo $$1; ?>', $text));
+
+    if ($this->nextSibling instanceof \DomText) {
+      $text .= ' ';
+    }
+    return ($text);
   }
 }
 ?>
